38ba96ce4e150d2730bda482d91671886b4e82d0
igor <igor@igor-ID49C>
2013-11-16 00:40:56 +0200

    hello

--- protected/components/Import/FreshbooksIterators/Invoices.php
+++ protected/components/Import/FreshbooksIterators/Invoices.php
@@ -6,51 +6,52 @@ use Components\Import\FreshbooksIterators\Base;
 class Invoices extends Base
 {
 	private $_date_from;
-	
+
 	public function __construct($date_from)
 	{
 		$this->_date_from = $date_from;
 	}
-	
+
 	protected function _onSuccess(array $response)
 	{
 		$invoices = $this->_prepareResult($response, 'invoices', 'invoice');
-		
+
 		$result_lines['lines']['line'] = array();
-		
+
 		foreach ($invoices as $invoice)
 		{
 			$lines = $this->_prepareResult($invoice, 'lines', 'line');
-			
+
 			foreach ($lines as $line)
 			{
 				if (!$line['name']) continue;
 				$line['date'] = $invoice['date'];
+				$line['unique_id'] = intval($invoice['invoice_id']).'-'.$line['line_id'];
 				$result_lines['lines']['line'][] = $line;
 			}
 		}
 
 		return $this->_prepareResult($result_lines, 'lines', 'line');
 	}
-	
+
 	protected function _convertData(array $data)
 	{
 		return array(
 			'user_id' => \Yii::app()->user->id,
 			'name' => $data['name'],
-			'foreign_id' => intval($data['line_id']),
+			'foreign_id' => $data['unique_id'],
 			'source_name' => 'freshbooks',
 			'amount' => $data['amount'],
 			'date' => $data['date']
 		);
 	}
-	
+
 	protected function _modifyPostData(array $data)
 	{
 		$data['date_from'] = $this->_date_from;
 		return $data;
 	}
-	
+
 	protected function _getFuncName()
 	{
 		return 'invoice.list';
--- protected/components/Import/LocalStorage.php
+++ protected/components/Import/LocalStorage.php
@@ -10,7 +10,7 @@ use Models\Import\LocalStorage as LocalStorageModel;
 class LocalStorage
 {
 	const BUNCH_LIMIT = 1000;
-	
+
 	public function save(BaseStrategy $strategy)
 	{
 		$this->_saveExpenses($strategy->getExpenses());
@@ -21,30 +21,30 @@ class LocalStorage
 	{
 		$this->_saveModel(new Expenses(), $data);
 	}
-	
+
 	private function _saveInvoices(\Iterator $data)
 	{
 		$this->_saveModel(new Invoices(), $data);
 	}
-	
+
 	private function _saveModel(LocalStorageModel $model, \Iterator $data)
 	{
 		$save_data = array();
 		$c = 0;
-		
+
 		foreach ($data as $row)
 		{
 			$save_data[] = $row;
 			$c++;
-			
+
 			if ($c == self::BUNCH_LIMIT)
 			{
 				$model->addBunch($save_data);
 				$save_data = array();
 				$c = 0;
-			}	
+			}
 		}
-		
+
 		if ($save_data)
 		{
 			$model->addBunch($save_data);
